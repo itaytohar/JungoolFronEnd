69f6baecdbb145de9294109d9658b6931ea50f4a
Itay Tohar <itay.tohar@gmail.com>
2022-02-07 15:08:29 +0200

    update urls

--- src/varaibles.ts
+++ src/varaibles.ts
@@ -1,14 +1,14 @@
 export const communityURL =
-  "https://customercommunitydetails202202.azurewebsites.net/api/CustomerCommunityDetails?code=Ws0YgZnBEMweQ8Niz2Kp7nmfcy0bxRTdfyk3KXmsaXzRCCRqmxvmXw==";
+  "https://customercommunitydetails202202.azurewebsites.net/api/CustomerCommunityDetails?&code=Ws0YgZnBEMweQ8Niz2Kp7nmfcy0bxRTdfyk3KXmsaXzRCCRqmxvmXw==";
 
 export const insightURL =
-  "https://insightdetails20220207135621.azurewebsites.net/api/InsightDetails?code=nEMfAibD56dtsOly085rHKKnVel7nF3Au2QmpCUlXgQr0cUxlflxqw==";
+  "https://insightdetails20220207135621.azurewebsites.net/api/InsightDetails?&code=nEMfAibD56dtsOly085rHKKnVel7nF3Au2QmpCUlXgQr0cUxlflxqw==";
 
 export const updateURL =
-  "https://updatecustomerorderstatus202202.azurewebsites.net/api/UpdateOrderStatus?code=Tz8mBLx8aALD3qcOZZUzplBpyCcNDjS9BmeTRfvYNOXVKrhVYYfg6w==";
+  "https://updatecustomerorderstatus202202.azurewebsites.net/api/UpdateOrderStatus?&code=Tz8mBLx8aALD3qcOZZUzplBpyCcNDjS9BmeTRfvYNOXVKrhVYYfg6w==";
 
 export const homeURL =
-  "https://customerinsights202203.azurewebsites.net/api/CustomerInsights?code=jhLa9eVkWLXHANbmobxFw5ssUtiI1OLqCqLqgVG0Ixy5Xdk7s0oCsw==";
+  "https://customerinsights202203.azurewebsites.net/api/CustomerInsights?&code=jhLa9eVkWLXHANbmobxFw5ssUtiI1OLqCqLqgVG0Ixy5Xdk7s0oCsw==";
 
 export const addOtherTaskURL =
-  "https://addothertasks202202.azurewebsites.net/api/AddOtherTasks?code=ZL5BtwdYuCgiw9J38xY5ceYV7fsLFoZItufYvtYEdExIpSG53rKmPA==";
+  "https://addothertasks202202.azurewebsites.net/api/AddOtherTasks?&code=ZL5BtwdYuCgiw9J38xY5ceYV7fsLFoZItufYvtYEdExIpSG53rKmPA==";
